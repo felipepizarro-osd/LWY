bc56a18e5a4b5ebe063adbad03cbd3bd0a9e3dbf
CriScarFace <67079157+CriScarFace@users.noreply.github.com>
2021-11-02 17:00:17 -0300

    Pantalla en Blanco

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,16 @@
+import 'package:first_app/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
-import './Question.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  var questionIndex = 0;
-
-  void responder() {
-    setState(() {
-      questionIndex = questionIndex + 1;
-    });
-
-    print(questionIndex);
-    //print("respuesta elegida !");
-  }
-
+class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
-    var question = [
-      'El cristian lo mataron los milicos?',
-      'Cual es tu animal Favorito'
-    ];
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Proyecto app'),
-        ),
-        body: Column(
-          children: <Widget>[
-            Question(
-              question[questionIndex],
-            ),
-            RaisedButton(
-              child: Text('Respuesta 1'),
-              onPressed: responder,
-            ),
-            RaisedButton(
-              child: Text('Respuesta 2'),
-              onPressed: responder,
-            ),
-            RaisedButton(
-              child: Text('Respuesta 3'),
-              onPressed: responder,
-            ),
-          ],
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      title: "C19Software",
+      home: SplashScreen(),
     );
   }
 }
