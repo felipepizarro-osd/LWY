0d586878e7a34f3e83865e64dae38d8c65421ec2
Carlos Tapia <68973311+c4rloncho@users.noreply.github.com>
2021-11-04 15:30:38 -0300

    Update: Cambio boton go!
    se cambia el boton antiguo por uno mas estetico

--- lib/src/pages/home_pages.dart
+++ lib/src/pages/home_pages.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/src/pages/perfil_usuario.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePages extends StatelessWidget {
   const HomePages({Key? key}) : super(key: key);
@@ -18,9 +19,20 @@ class HomePages extends StatelessWidget {
               'assets/imgs/c19.png',
               height: 500,
             ),
-            IconButton(
-              iconSize: 100,
-              icon: SvgPicture.asset('assets/icons/go.svg'),
+            ElevatedButton(
+              child: Text(
+                'Go!',
+                style: GoogleFonts.baloo(
+                  fontSize: 35,
+                ),
+              ),
+              style: ButtonStyle(
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0),
+                  ),
+                ),
+              ),
               onPressed: () {
                 Navigator.pushNamed(context, "home");
               },
